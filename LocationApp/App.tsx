1c774817e50a0cf4b32f566816bc23b6a33a3dc0
joewgeorger <joewgeorger@wustl.edu>
2023-03-22 17:22:49 -0500

    started AsyncStorage

--- LocationApp/App.tsx
+++ LocationApp/App.tsx
@@ -7,6 +7,7 @@
 
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import type { PropsWithChildren } from 'react';
 import {
   Text,
@@ -25,6 +26,32 @@ import ShowMap from './src/Maps/ShowMap';
 function App(): JSX.Element {
   // console.log(usersCollection);
   // push();
+  console.log(`Asyncstorage test`)
+  const _storeData = async () => {
+    try {
+      await AsyncStorage.setItem(
+        `uid`, 
+        `test`
+      );
+    } catch (error) {
+      console.log(`Error: ${error}`)
+    }
+  };
+  const _retrieveData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('uid');
+        if (value !== null) {
+          // We have data!!
+          console.log(value);
+        }
+        else{
+          console.log("No data");
+        }
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+  };
+    console.log(`test set: ${_retrieveData}`)
   return (
     <ShowMap />
   );
